f236074cfddc935fa3025a722f0e1a45ff08ea3d
romulosouza <romulovinicius10@gmail.com>
2020-09-16 00:16:58 -0300

    Added initial colision method

--- classes/syringe.py
+++ classes/syringe.py
@@ -11,29 +11,17 @@ class Syringe(Sprite):
         self.start_x = constants.SYRINGE_START_X
         self.start_y = constants.SYRINGE_START_Y
         self.pos_y = (pyxel.height - self.height) / 2
+        self.name = name
 
         if name == 'p1':
             self.pos_x = 0
             self.key_up = pyxel.KEY_W
             self.key_down = pyxel.KEY_S
 
-            # syringe divided in 2 rectangles
-            # (x, (begin_y, end_y))
-            self.hitbox = [
-                (self.width-4, (self.pos_y, self.pos_y+24)),
-                (self.width, (self.pos_y+25, self.pos_y+self.height))
-            ]
-
         elif name == 'p2':
             self.pos_x = pyxel.width - self.width
             self.key_up = pyxel.KEY_UP
             self.key_down = pyxel.KEY_DOWN
-
-            screen = pyxel.width
-            self.hitbox = [
-                (screen-self.width+4, (self.pos_y, self.pos_y+24)),
-                (screen-self.width, (self.pos_y+25, self.pos_y+self.height))
-            ]
 
     def update(self):
         if pyxel.btn(self.key_up) and self.pos_y > 0:
@@ -42,3 +30,18 @@ class Syringe(Sprite):
         elif pyxel.btn(self.key_down):
             if self.pos_y < pyxel.height-self.height:
                 self.pos_y += constants.SYRINGE_SPEED
+
+        self.update_hitbox()
+
+    def update_hitbox(self):
+        if self.name == 'p1':
+            self.hitbox = [
+                (self.width-4, (self.pos_y, self.pos_y+24)),
+                (self.width, (self.pos_y+25, self.pos_y+self.height))
+            ]
+        elif self.name == 'p2':
+            screen = pyxel.width
+            self.hitbox = [
+                (screen-self.width+4, (self.pos_y, self.pos_y+24)),
+                (screen-self.width, (self.pos_y+25, self.pos_y+self.height))
+            ]
--- classes/virus.py
+++ classes/virus.py
@@ -20,21 +20,47 @@ class Virus(Sprite):
         self.speed_x = 0
         self.speed_y = 0
 
-        self.hitbox = [
-            (self.pos_x+2, (self.pos_y+2, self.pos_y+4)),
-            (self.pos_x+self.width-2, (self.pos_y+2, self.pos_y+4))
-        ]
-
-        self.initial_setup(random.choice([player1, player2]))
+        self.reset_virus(random.choice([player1, player2]))
 
     def update(self):
         self.pos_x += self.speed_x
         self.pos_y += self.speed_y
 
-    def check_colision(self):
-        ...
+        self.update_hitbox()
 
-    def initial_setup(self, player):
+        if self.speed_x > 0:
+            if self.check_colision(self.player2):
+                self.speed_x *= -1
+                self.speed_y *= -1
+        else:
+            if self.check_colision(self.player1):
+                self.speed_x *= -1
+                self.speed_y *= -1
+
+    def check_colision(self, player):
+        for s_hbox in player.syringe.hitbox:
+            if player.name == 'p1':
+                v_hitbox = self.hitbox[0]
+                if v_hitbox[0] > float(s_hbox[0]):
+                    continue
+            elif player.name == 'p2':
+                v_hitbox = self.hitbox[1]
+                if v_hitbox[0] < float(s_hbox[0]):
+                    continue
+
+            for v_hb in range(round(v_hitbox[1][0]), round(v_hitbox[1][1])):
+                if s_hbox[1][0] <= v_hb <= s_hbox[1][1]:
+                    return True
+
+        return False
+
+    def update_hitbox(self):
+        self.hitbox = [
+            (self.pos_x+2, (self.pos_y+2, self.pos_y+2+4)),  # p1 colision
+            (self.pos_x+self.width-2, (self.pos_y+2, self.pos_y+2+4))
+        ]
+
+    def reset_virus(self, player):
         initial_height = random.choice([0, pyxel.height-self.height])
         self.speed_x = constants.VIRUS_SPEED_X
         self.speed_y = constants.VIRUS_SPEED_Y
