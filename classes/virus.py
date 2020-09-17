4c65c2de13e28bd63a60900db661842462b31586
romulosouza <romulovinicius10@gmail.com>
2020-09-17 11:57:46 -0300

    Reorganized classes

--- classes/virus.py
+++ classes/virus.py
@@ -5,10 +5,7 @@ import pyxel
 
 
 class Virus(Sprite):
-    def __init__(self, player1, player2):
-        self.player1 = player1
-        self.player2 = player2
-
+    def __init__(self):
         self.width = constants.VIRUS_WIDTH
         self.height = constants.VIRUS_HEIGHT
         self.img_idx = constants.VIRUS_IDX
@@ -20,43 +17,14 @@ class Virus(Sprite):
         self.speed_x = constants.VIRUS_MIN_SPEED_X
         self.speed_y = constants.VIRUS_MIN_SPEED_Y
 
-        self.reset_virus(random.choice([player1, player2]))
-
     def update(self):
         self.pos_x += self.speed_x
         self.pos_y += int(self.speed_y)
 
         self.update_hitbox()
 
-        self.check_screen_border()
-
-        if self.speed_x > 0:
-            if self.check_colision(self.player2):
-                self.update_speed(self.player2)
-        else:
-            if self.check_colision(self.player1):
-                self.update_speed(self.player1)
-
-    def check_screen_border(self):
-        if self.pos_y <= 0 or self.pos_y >= pyxel.height-self.width:
-            self.speed_y *= -1
-
-    def check_colision(self, player):
-        for s_hbox in player.syringe.hitbox:
-            if player.name == 'p1':
-                v_hitbox = self.hitbox[0]
-                if v_hitbox[0] > float(s_hbox[0]):
-                    continue
-            elif player.name == 'p2':
-                v_hitbox = self.hitbox[1]
-                if v_hitbox[0] < float(s_hbox[0]):
-                    continue
-
-            for v_hb in range(round(v_hitbox[1][0]), round(v_hitbox[1][1])):
-                if s_hbox[1][0] <= v_hb <= s_hbox[1][1]:
-                    return True
-
-        return False
+    def invert_speed_y_direction(self):
+        self.speed_y *= -1
 
     def update_speed(self, player):
         both_up = self.speed_y < 0 and pyxel.btn(player.syringe.key_up)
@@ -85,13 +53,12 @@ class Virus(Sprite):
         initial_height = random.choice([0, pyxel.height-self.height])
         self.speed_x = constants.VIRUS_MIN_SPEED_X
         self.speed_y = constants.VIRUS_MIN_SPEED_Y
+        self.pos_y = initial_height
         if initial_height:
             self.speed_y *= -1
 
         if player.name == 'p1':
             self.pos_x = player.syringe.width
-            self.pos_y = initial_height
         else:
             self.pos_x = pyxel.width - player.syringe.width
-            self.pos_y = initial_height
             self.speed_x *= -1
--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 from classes.player import Player
 from classes.virus import Virus
+import random
 import pyxel
 
 
@@ -8,7 +9,9 @@ class App:
         pyxel.init(256, 150, caption='Corong', fps=60)
         self.player1 = Player('p1')
         self.player2 = Player('p2')
-        self.virus = Virus(self.player1, self.player2)
+
+        self.virus = Virus()
+        self.virus.reset_virus(random.choice([self.player1, self.player2]))
 
         pyxel.load('assets/sprites.pyxres')
         pyxel.run(self.update, self.draw)
@@ -18,11 +21,42 @@ class App:
         self.player2.update()
         self.virus.update()
 
+        self.check_screen_border()
+
+        if self.virus.speed_x > 0:
+            if self.check_colision(self.player2):
+                self.virus.update_speed(self.player2)
+        else:
+            if self.check_colision(self.player1):
+                self.virus.update_speed(self.player1)
+
     def draw(self):
         pyxel.cls(0)
         self.player1.draw()
         self.player2.draw()
         self.virus.draw()
+
+    def check_screen_border(self):
+        virus = self.virus
+        if virus.pos_y <= 0 or virus.pos_y >= pyxel.height-virus.width:
+            virus.invert_speed_y_direction()
+
+    def check_colision(self, player):
+        for s_hbox in player.syringe.hitbox:
+            if player.name == 'p1':
+                v_hitbox = self.virus.hitbox[0]
+                if v_hitbox[0] > float(s_hbox[0]):
+                    continue
+            elif player.name == 'p2':
+                v_hitbox = self.virus.hitbox[1]
+                if v_hitbox[0] < float(s_hbox[0]):
+                    continue
+
+            for v_hb in range(round(v_hitbox[1][0]), round(v_hitbox[1][1])):
+                if s_hbox[1][0] <= v_hb <= s_hbox[1][1]:
+                    return True
+
+        return False
 
 
 if __name__ == '__main__':
